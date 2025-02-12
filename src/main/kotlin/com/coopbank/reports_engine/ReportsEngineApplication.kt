5bf9383e93506955506ab1e0f0afcf069b5d9078
Peter Chege <62762943+chege4179@users.noreply.github.com>
2025-02-12 14:49:24 +0300

    first commit

---
+++ src/main/kotlin/com/coopbank/reports_engine/ReportsEngineApplication.kt
@@ -0,0 +1,11 @@
+package com.coopbank.reports_engine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportsEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReportsEngineApplication>(*args)
+}
---
+++ src/test/kotlin/com/coopbank/reports_engine/ReportsEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coopbank.reports_engine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReportsEngineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
